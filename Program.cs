ac30e5eb4d6907cb9df4c2eb06494b7de408c077
tmsmith33 <157940181+tmsmith33@users.noreply.github.com>
2024-09-09 10:51:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿const double COST_OF_SANDWICH = 4.75;
+const double COST_OF_TOPPING = 0.55;
+const double DISCOUNT_AMOUNT = 0.10;
+
+System.Console.WriteLine("How many sandwiches did you buy?");
+int numberOfSandwiches = int.Parse(Console.ReadLine());
+double totalSandwichCost = COST_OF_SANDWICH * numberOfSandwiches;
+
+System.Console.WriteLine("How many toppings were needed?");
+int numberOfToppings = int.Parse(Console.ReadLine());
+double totalToppingCost = COST_OF_TOPPING * numberOfToppings;
+
+System.Console.WriteLine("How much would you like to tip?");
+double tip = double.Parse(Console.ReadLine());
+
+double baseCost = totalSandwichCost + totalToppingCost;
+
+double orderCost = (baseCost * (1-DISCOUNT_AMOUNT)) + tip;
+
+System.Console.WriteLine("The order cost is " + orderCost);
